36bcf59d12113501d8329f8d1dfa32c1f723cbb1
Vladimir Bukvić <vladimir.bukvic@live.com>
2015-05-09 13:57:56 +0000

    Auction model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
     belongs_to :user
+    has_one :auction # ovime smo dobili pristup auction metodu dole
+    
+    def has_auction?
+        auction.present?
+        # u tom smislu, sa recju "auction" idemo do relationshipa
+    end
 end
